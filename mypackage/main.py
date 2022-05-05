291874a2bcfd1db276e9f71140105aa5a141c822
Will Johnson <will@willj.co>
2022-05-04 19:15:07 -0500

    Adding setup.py

---
+++ mypackage/main.py
@@ -0,0 +1,2 @@
+def do_a_thing():
+    return 123
---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='mypackage',
+    version='0.0.3',
+    description='Collection of Utilities',
+    url='https://willj.co',
+    packages=['mypackage'],
+    author='will@willj.co',
+    zip_safe=False
+)
